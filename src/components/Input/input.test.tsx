bbd7933097e6d726d5b209244df2f70e5f707057
wagnermateus <wagnermateus7@gmail.com>
2023-08-15 23:45:50 +0100

    Teste de Componentes - Debug do render

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -2,6 +2,8 @@ import { render } from "@testing-library/react-native";
 import { Input } from ".";
 describe("Component: Input", () => {
   it("should  be render without activity indicator", () => {
-    render(<Input />);
+    const { debug } = render(<Input />);
+
+    debug();
   });
 });
