753433b76145b1e86d13c2c5781530a6392909db
wagnermateus <wagnermateus7@gmail.com>
2023-08-16 00:01:21 +0100

    Teste de Componentes - Entendendo o Get e o Query

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,11 +1,11 @@
 import { render, screen } from "@testing-library/react-native";
 import { Input } from ".";
 describe("Component: Input", () => {
-  it("should  be render without activity indicator", () => {
-    render(<Input isLoading />);
+  it("should  be render without activity indicator if isLoading prop is undefined", () => {
+    render(<Input />);
 
-    const activityIndicator = screen.getByTestId("activity-indicator");
+    const activityIndicator = screen.queryByTestId("activity-indicator");
 
-    console.log(activityIndicator);
+    expect(activityIndicator).toBe(null);
   });
 });
