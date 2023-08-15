c6627d604e7a853d27f7bf01be8f59d75b451c0d
wagnermateus <wagnermateus7@gmail.com>
2023-08-16 00:36:06 +0100

    Mocks - GetByText

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react-native";
+
+import { SelectList } from "@components/SelectList";
+
+describe("Component: SelectList", () => {
+  it("should be return city details selected", async () => {
+    const data = [
+      { id: "1", name: "Nova Vida", latitude: 123, longitude: 456 },
+      { id: "2", name: "Talatona", latitude: 123, longitude: 456 },
+    ];
+
+    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />);
+
+    const selectedCity = screen.getByText("Nova Vida");
+    console.log(selectedCity);
+  });
+});
