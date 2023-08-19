353de6b784135d60c91834c9a340dc29fb7ae2bf
wagnermateus <wagnermateus7@gmail.com>
2023-08-19 21:38:17 +0100

    Mocks - Testando rotorno da API

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -2,7 +2,7 @@ import { mockCityAPIResponse } from "@__tests__/mocks/mockCityAPIResponse";
 import { api } from "./api";
 import { getCityByNameService } from "./getCityByNameService";
 
-describe("api: getCityByNameService", () => {
+describe("Service: getCityByNameService", () => {
   it("should return city details", async () => {
     jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse });
 
