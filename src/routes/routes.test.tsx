21eabdd32d94c4727a009c18556140b21da9a27c
wagnermateus <wagnermateus7@gmail.com>
2023-08-20 01:56:42 +0100

    Testando Hooks - Finalizando o teste das rotas

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,6 +1,9 @@
-import { render, screen, waitFor } from "@testing-library/react-native";
+import { render } from "@__tests__/utils/customRender";
+import { screen, waitFor, act } from "@testing-library/react-native";
 import { Routes } from ".";
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { api } from "@services/api";
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
 
 describe("Routes", () => {
   it("should be render Search screen when not city selected", async () => {
@@ -11,7 +14,9 @@ describe("Routes", () => {
     expect(title).toBeTruthy();
   });
 
-  it("shoul be render Dashboard screen when has city selected", async () => {
+  it("should be render Dashboard screen when has city selected", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
+
     const city = {
       id: "1",
       name: "São Paulo",
@@ -21,7 +26,10 @@ describe("Routes", () => {
 
     await saveStorageCity(city);
 
-    const { debug } = render(<Routes />);
-    debug();
+    await act(() => waitFor(() => render(<Routes />)));
+
+    const title = screen.getByText(city.name);
+
+    expect(title).toBeTruthy();
   });
 });
