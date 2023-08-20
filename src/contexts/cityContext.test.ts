6ecd5ea311e0a6c34aad701986818e217ac15622
wagnermateus <wagnermateus7@gmail.com>
2023-08-20 01:18:52 +0100

    Testando Hooks - Atualizando o estado

--- src/contexts/cityContext.test.ts
+++ src/contexts/cityContext.test.ts
@@ -7,6 +7,17 @@ describe("Context: cityContext", () => {
   it("should be change selected city", async () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
 
-    await waitFor(() => act(() => console.log(result.current)));
+    await waitFor(() =>
+      act(() =>
+        result.current.handleChanceCity({
+          id: "1",
+          name: "Luanda",
+          latitude: 123,
+          longitude: 456,
+        })
+      )
+    );
+
+    expect(result.current.city?.name).toBe("Luanda");
   });
 });
